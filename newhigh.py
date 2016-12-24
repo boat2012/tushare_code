80d1fe0e2f99b887f41d27408914e806a31a8c50
boat1 <boat1@tom.com>
2016-12-24 17:01:27 +0800

    2016-12-24

--- newhigh.py
+++ newhigh.py
@@ -2,16 +2,24 @@
 __author__ = 'rocky'  
 #获取破指定天数内的新高 比如破60日新高  
 import tushare as ts  
-import datetime  
+import datetime
+import os
 info=ts.get_stock_basics()  
-  
-def loop_all_stocks():  
-    for EachStockID in info.index:  
+filename="1224.csv"
+def loop_all_stocks():
+    print u"总共有",len(info),u"支股票\n"
+    i = 0
+    for EachStockID in info.index:
+         i = i + 1;
          if is_break_high(EachStockID,60):  
-             print "High price on",  
+             print u"第",i,u"支股票High price on",
              print EachStockID,  
-             print info.ix[EachStockID]['name'].decode('utf-8')  
-  
+             print info.ix[EachStockID]['name'].decode('utf-8')
+             if os.path.exists(filename):
+                info.ix[EachStockID].to_frame(name=EachStockID).T.to_csv(filename, mode='a', encoding="utf8",header=None)
+             else:
+                info.ix[EachStockID].to_frame(name=EachStockID).T.to_csv(filename,encoding="utf8")
+
   
   
 def is_break_high(stockID,days):  
@@ -23,16 +31,18 @@ def is_break_high(stockID,days):
     start_day=start_day.strftime("%Y-%m-%d")  
     end_day=end_day.strftime("%Y-%m-%d")  
     df=ts.get_k_data(stockID,start=start_day,end=end_day)  
-  
-    period_high=df['high'].max()  
-    #print period_high  
-    today_high=df.iloc[0]['high']  
-    #这里不能直接用 .values  
-    #如果用的df【：1】 就需要用.values  
-    #print today_high  
-    if today_high>=period_high:  
-        return True  
-    else:  
-        return False  
+    if len(df) > 30 :   # 上市一个月以内的不考虑
+        period_high=df['high'].max()
+        #print period_high
+        today_high=df.iloc[-1]['high']
+        #这里不能直接用 .values
+        #如果用的df【：1】 就需要用.values
+        #print today_high
+        if today_high>=period_high:
+            return True
+        else:
+            return False
+    else:
+        return False
   
 loop_all_stocks()  
