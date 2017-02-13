8f9c80a286809fc98b908d8bf85ad82bbda530bf
boat2012 <boat1@tom.com>
2017-02-13 17:04:55 +0800

    0213

--- pyal/constant.py
+++ pyal/constant.py
@@ -6,6 +6,8 @@ Created on Sat Aug 01 17:28:29 2015
 from datetime import datetime
 import pandas as pd
 import sys
+from email.mime.text import MIMEText
+import smtplib
 """
 时间常数
 """
@@ -17,7 +19,29 @@ _end_range = pd.date_range(end=END,periods=7)
 
 _RATE_FREE_ = 0.05
 
-
+###############################################
+mailto_list=["3223449@qq.com"]
+mail_host="smtp.tom.com"  #设置服务器
+mail_user="fz.sea"    #用户名
+mail_pass="boat1.2012"   #口令
+mail_postfix="tom.com"  #发件箱的后缀
+################################################
+def send_mail(to_list = mailto_list,sub,content):  # to_list：收件人；sub：主题；content：邮件内容
+    me="ITV Daily Report"+"<"+mail_user+"@"+mail_postfix+">"   #这里的hello可以任意设置，收到信后，将按照设置显示
+    msg = MIMEText(content,_subtype='html',_charset='utf-8')    #创建一个实例，这里设置为html格式邮件
+    msg['Subject'] = sub    #设置主题
+    msg['From'] = me
+    msg['To'] = ";".join(to_list)
+    try:
+        s = smtplib.SMTP()
+        s.connect(mail_host)  #连接smtp服务器
+        s.login(mail_user,mail_pass)  #登陆服务器
+        s.sendmail(me, to_list, msg.as_string())  #发送邮件
+        s.close()
+        return True
+    except Exception, e:
+        print str(e)
+        return False
 
 """
 数据库常数
